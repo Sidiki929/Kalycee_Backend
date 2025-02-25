803c49c37b3bdd98be8d36f1d1f129b49d9adb42
Sidiki Dembélé <dsidiki@jway.ma>
2025-02-25 14:24:43 +0000

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -273,6 +273,7 @@ const loginUser = asyncHandler(async (req, res) => {
     _id: updatedUser._id,
     prenom: updatedUser.prenom,
     nom: updatedUser.nom,
+    telephone:updateUser.telephone,
     userType: updatedUser.userType,
     date_ajout: updatedUser.date_ajout,
     token: generateToken(updatedUser._id),
